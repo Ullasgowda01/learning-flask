21411e743773d8b38e6d2c8fb40443602ca0a9c2
Ullasgowda01 <Ullasgowdaullu@gmail.com>
2025-09-20 17:14:58 +0530

    new page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,3 +6,14 @@ app = Flask(__name__)
 def home():
     return 'welcome to flask'
 
+@app.route('/aboutUs')
+def aboutUs():
+    return 'about me '
+
+@app.route('/submit', methods = ['POST', 'GET'])
+def submit():
+    if request.method == 'POST':
+        return 'data sent'
+    else :
+        return 'user viewed '
+    
